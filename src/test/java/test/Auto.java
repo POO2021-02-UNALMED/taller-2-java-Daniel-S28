e045b2131ec30b257057c53b0f61c862e4af8b2e
Daniel-S28 <80227692+Daniel-S28@users.noreply.github.com>
2021-10-19 14:21:29 -0500

    Add files via upload

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,35 @@
+//package test;
+public class Auto {
+
+	String modelo;
+	int precio;
+	Asiento [] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	int cantidadAsientos() {
+		int n=0;
+		for (int i=0; i<asientos.length; i++) {
+			if (asientos[i]!=null) {
+				n++;
+			}
+		}
+		return n;
+	}
+	
+	String verificarIntegridad() {
+		if (this.registro!=motor.registro) {
+			return "Las piezas no son originales";
+		}
+		else {
+			for (int i=0; i<asientos.length; i++) {
+				if (asientos[i].registro!=this.registro) {
+					return "Las piezas no son originales";
+				}
+			}
+			return "Auto original";
+		}
+	}
+}
